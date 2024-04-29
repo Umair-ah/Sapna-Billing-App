acace72486100d766061edd82837d239d9c1fef1
Umair Ahmed <umairahmed200203.umair@gmail.com>
2024-04-29 18:48:12 +0530

    invoice show page designed

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,70 +1,58 @@
 class InvoicesController < ApplicationController
   before_action :set_invoice, only: %i[ show edit update destroy ]
   before_action :authenticate_admin!
-  # GET /invoices or /invoices.json
+
   def index
     @invoices = Invoice.all
   end
 
-  # GET /invoices/1 or /invoices/1.json
   def show
   end
 
-  # GET /invoices/new
   def new
     @invoice = Invoice.new
+    @invoice.items.build
   end
 
-  # GET /invoices/1/edit
   def edit
   end
 
-  # POST /invoices or /invoices.json
   def create
     @invoice = Invoice.new(invoice_params)
 
     respond_to do |format|
       if @invoice.save
         format.html { redirect_to invoice_url(@invoice), notice: "Invoice was successfully created." }
-        format.json { render :show, status: :created, location: @invoice }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @invoice.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /invoices/1 or /invoices/1.json
   def update
     respond_to do |format|
       if @invoice.update(invoice_params)
         format.html { redirect_to invoice_url(@invoice), notice: "Invoice was successfully updated." }
-        format.json { render :show, status: :ok, location: @invoice }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @invoice.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /invoices/1 or /invoices/1.json
   def destroy
     @invoice.destroy
 
     respond_to do |format|
       format.html { redirect_to invoices_url, notice: "Invoice was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_invoice
       @invoice = Invoice.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def invoice_params
-      params.require(:invoice).permit(:number, :date, :to, :total_amount)
+      params.require(:invoice).permit(:number, :date, :to, :total_amount, items_attributes: [:id, :description, :rate, :quantity, :amount,:_destroy])
     end
 end
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,15 @@
 module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        'sm': '640px',  // Add your custom small breakpoint here
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+    },
+  },
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',

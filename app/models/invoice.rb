dc594117b36210557afd1f3dab0490f991ce56e6
Umair Ahmed <umairahmed200203.umair@gmail.com>
2024-04-29 19:28:33 +0530

    index page also designed and dependent destroy

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,4 @@
 class Invoice < ApplicationRecord
-  has_many :items, inverse_of: :invoice
+  has_many :items, inverse_of: :invoice, dependent: :destroy
   accepts_nested_attributes_for :items, reject_if: :all_blank, allow_destroy: true
 end

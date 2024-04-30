472bd6c88279e263ce41516fe271ee4c430b3580
Umair Ahmed <umairahmed200203.umair@gmail.com>
2024-04-30 14:33:58 +0530

    table for invocie

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,13 @@
 class Invoice < ApplicationRecord
   has_many :items, inverse_of: :invoice, dependent: :destroy
   accepts_nested_attributes_for :items, reject_if: :all_blank, allow_destroy: true
+
+
+  after_create :calc_total_amt
+  after_update :calc_total_amt
+
+
+  def calc_total_amt
+    update_column :total_amount, items.map(&:amount).sum
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :invoice
+  
+  after_create :calc_amt
+  after_update :calc_amt
+  
+
+  
+
+  def calc_amt
+    update_column :amount, (self.rate * self.quantity)
+  end
 end
